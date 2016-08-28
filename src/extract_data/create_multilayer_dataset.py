8bdaf7c5b143571b511ef6068d88a0e9769d0a03
David <davsuacar@gmail.com>
2016-08-29 00:22:32 +0200

    Multilayer dataset

---
+++ src/extract_data/create_multilayer_dataset.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import cv2
+import sys
+import os
+import dlib
+import glob
+import csv
+from skimage import io
+import numpy
+import util.distances as distances
+
+numpy.set_printoptions(threshold=numpy.nan)
+
+if len(sys.argv) != 3:
+    print(
+        "Give the path to the trained shape predictor model as the first "
+        "argument and then the directory containing the facial images.\n"
+        "For example, if you are in the python_examples folder then "
+        "execute this program by running:\n"
+        "    ./face_landmark_detection.py shape_predictor_68_face_landmarks.dat ../examples/faces\n"
+        "You can download a trained facial shape predictor from:\n"
+        "    http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2")
+    exit()
+
+predictor_path = sys.argv[1]
+faces_folder_path = sys.argv[2]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+win = dlib.image_window()
+
+with open('../../data/multilayer/data.csv', 'wb') as csvdata:
+
+    dataset = csv.writer(csvdata, delimiter=':',
+                            quotechar='\t', quoting=csv.QUOTE_MINIMAL)
+
+    i = 0
+    for subdir, dirs, files in os.walk(faces_folder_path):
+
+        for f in glob.glob(os.path.join(subdir, "*.jpg")):
+
+            print os.path.join(subdir, f)
+
+
+            img = io.imread(f)
+
+            dets = detector(img, 1)
+
+            for k, d in enumerate(dets):
+                # Get the landmarks/parts for the face in box d.
+                shape = predictor(img, d)
+
+                points = [y for k in range(68) for y in [(shape.part(k).x, shape.part(k).y)]]
+
+                input_data = distances.calculate_proportions(points)
+                input_data.append(i)
+
+                dataset.writerow(input_data)
+
+        i += 1
--- src/extract_data/take_faces.py
+++ src/extract_data/take_faces.py
@@ -15,25 +15,8 @@ while True:
     i += 1
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    backtorgb = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
 
-    win.clear_overlay()
-    win.set_image(backtorgb)
-
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
-    dets = detector(backtorgb)
-
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(k, d.left(), d.top(), d.right(), d.bottom()))
-
-        win.add_overlay(dets)
-        cropped = backtorgb[d.top(): d.top() + d.bottom(), d.left():d.right() + d.left()]
-        cv2.imwrite("../img/image_" + str(i) + ".jpg", cropped)
-    # Display the resulting frame
-    # cv2.imshow('Video', frame)
+    cv2.imwrite("../img/image_" + str(i) + ".jpg", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
